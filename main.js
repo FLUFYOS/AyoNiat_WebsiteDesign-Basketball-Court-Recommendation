8da009d9238220366d73c55580ed4a8c7683ee0a
arvinr16 <arviansyahyk@gmail.com>
2025-08-21 22:24:38 +0700

    Adding 'hamburger' menu (finish)

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+const navbar = document.getElementById('navbar');
 const hamburger = document.getElementById('hamburger');
 const navLink = document.getElementById('nav-link');
 
 // Alihkan buka/tutup menu
 hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active')
     hamburger.classList.toggle('active');
     navLink.classList.toggle('open');
 });
